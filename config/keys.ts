b64235203d8872a2fc34b572bc18075136597497
lukaduma123 <luka.dumancic49@gmail.com>
2019-08-24 22:30:40 +0200

    deploy

--- config/keys.ts
+++ config/keys.ts
@@ -2,5 +2,5 @@ import prod from './prod';
 //import dev from './dev';
 
 //const keys: any = process.env.NODE_ENV === 'production' ? prod : dev;
-const keys: any;
-export default prod;
+const keys: any = prod;
+export default keys;
